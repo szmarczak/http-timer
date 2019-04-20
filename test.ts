c4778f13eea601979aa9d20b28071342b7f88f42
Szymon Marczak <sz.marczak@gmail.com>
2019-04-20 19:59:28 +0200

    fixes

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import {AddressInfo} from 'net';
 import http, {ClientRequest} from 'http';
 import https from 'https';
 import util from 'util';
-import {URL, parse as parseUrl} from 'url';
+import {URL, parse as parseUrl} from 'url'; // eslint-disable-line node/no-deprecated-api
 import EventEmitter from 'events';
 import test from 'ava';
 import pEvent from 'p-event';
@@ -10,6 +10,8 @@ import timer, {Timings} from './source';
 
 let server: http.Server & {
 	url?: string;
+	listenAsync?: any;
+	closeAsync?: any;
 };
 
 test.before('setup', async () => {
@@ -23,15 +25,15 @@ test.before('setup', async () => {
 		}
 	});
 
-	server.listen = util.promisify(server.listen.bind(server));
-	server.close = util.promisify(server.close.bind(server));
+	server.listenAsync = util.promisify(server.listen.bind(server));
+	server.closeAsync = util.promisify(server.close.bind(server));
 
-	await server.listen();
+	await server.listenAsync();
 	server.url = `http://127.0.0.1:${(server.address() as AddressInfo).port}`;
 });
 
 test.after('cleanup', async () => {
-	await server.close();
+	await server.closeAsync();
 });
 
 const error = 'Simple error';
@@ -89,13 +91,13 @@ test('phases', async t => {
 	t.is(typeof timings.phases.download, 'number');
 	t.is(typeof timings.phases.total, 'number');
 
-	t.is(timings.phases.wait, timings.socket - timings.start);
-	t.is(timings.phases.dns, timings.lookup - timings.socket);
-	t.is(timings.phases.tcp, timings.connect - timings.lookup);
-	t.is(timings.phases.request, timings.upload - timings.connect);
-	t.is(timings.phases.firstByte, timings.response - timings.upload);
-	t.is(timings.phases.download, timings.end - timings.response);
-	t.is(timings.phases.total, timings.end - timings.start);
+	t.is(timings.phases.wait, timings.socket! - timings.start);
+	t.is(timings.phases.dns, timings.lookup! - timings.socket!);
+	t.is(timings.phases.tcp, timings.connect! - timings.lookup!);
+	t.is(timings.phases.request, timings.upload! - timings.connect!);
+	t.is(timings.phases.firstByte, timings.response! - timings.upload!);
+	t.is(timings.phases.download, timings.end! - timings.response!);
+	t.is(timings.phases.total, timings.end! - timings.start);
 });
 
 test('no memory leak (`lookup` event)', async t => {
@@ -121,7 +123,7 @@ test('sets `total` on request error', async t => {
 	t.is(err.message, 'socket hang up');
 
 	t.is(typeof timings.error, 'number');
-	t.is(timings.phases.total, timings.error - timings.start);
+	t.is(timings.phases.total, timings.error! - timings.start);
 });
 
 test('sets `total` on response error', async t => {
@@ -133,11 +135,11 @@ test('sets `total` on response error', async t => {
 	const timings = timer(request);
 
 	const response = await pEvent(request, 'response');
-	const err = await pEvent(response, 'error');
+	const err: Error = await pEvent(response, 'error');
 
 	t.is(err.message, error);
 	t.is(typeof timings.error, 'number');
-	t.is(timings.phases.total, timings.error - timings.start);
+	t.is(timings.phases.total, timings.error! - timings.start);
 });
 
 test('doesn\'t throw when someone used `.prependOnceListener()`', async t => {
@@ -156,17 +158,17 @@ test('sensible timings', async t => {
 	response.resume();
 	await pEvent(response, 'end');
 
-	t.true(timings.socket >= now);
-	t.true(timings.lookup >= now);
-	t.true(timings.connect >= now);
-	t.true(timings.response >= now);
-	t.true(timings.end >= now);
+	t.true(timings.socket! >= now);
+	t.true(timings.lookup! >= now);
+	t.true(timings.connect! >= now);
+	t.true(timings.response! >= now);
+	t.true(timings.end! >= now);
 	t.is(timings.error, undefined);
-	t.true(timings.phases.wait < 1000);
-	t.true(timings.phases.dns < 1000);
-	t.true(timings.phases.tcp < 1000);
-	t.true(timings.phases.request < 1000);
-	t.true(timings.phases.firstByte < 1000);
-	t.true(timings.phases.download < 1000);
-	t.true(timings.phases.total < 1000);
+	t.true(timings.phases.wait! < 1000);
+	t.true(timings.phases.dns! < 1000);
+	t.true(timings.phases.tcp! < 1000);
+	t.true(timings.phases.request! < 1000);
+	t.true(timings.phases.firstByte! < 1000);
+	t.true(timings.phases.download! < 1000);
+	t.true(timings.phases.total! < 1000);
 });
